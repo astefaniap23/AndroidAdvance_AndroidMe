a2f0435a23cf68fc4d4eedd3c35291afee705c68
Alba Portillo <stefania23p@gmail.com>
2017-05-30 13:30:25 -0400

    Exercise 06: CommunicateBetweenActivities

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.android_me.ui;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +12,13 @@ import com.example.android.android_me.data.AndroidImageAssets;
 public class AndroidMeActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@NonNull Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+        Bundle bundleIntent = getIntent().getExtras();
+        int headIndex = bundleIntent.getInt("headIndex");
+        int bodyIndex = bundleIntent.getInt("bodyIndex");
+        int legIndex = bundleIntent.getInt("legIndex");
 
         if (savedInstanceState == null) {
             //Fragmente transaction
@@ -21,21 +26,21 @@ public class AndroidMeActivity extends AppCompatActivity {
 
             BodyPartFragment headFragment = new BodyPartFragment();
             headFragment.setImageIds(AndroidImageAssets.getHeads());
-            headFragment.setListIndex(2);
+            headFragment.setListIndex(headIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.head_container, headFragment)
                     .commit();
 
             BodyPartFragment bodyFragment = new BodyPartFragment();
             bodyFragment.setImageIds(AndroidImageAssets.getBodies());
-            //   bodyFragment.setListIndex(2);
+            bodyFragment.setListIndex(bodyIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.body_container, bodyFragment)
                     .commit();
 
             BodyPartFragment legsFragment = new BodyPartFragment();
             legsFragment.setImageIds(AndroidImageAssets.getLegs());
-            //    legsFragment.setListIndex(2);
+            legsFragment.setListIndex(legIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.legs_container, legsFragment)
                     .commit();
--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -12,6 +15,10 @@ import com.example.android.android_me.R;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +27,41 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
     @Override
     public void onImageSelected(int position) {
-        Toast.makeText(this,"Position clicked ="+position,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Position clicked =" + position, Toast.LENGTH_SHORT).show();
+
+        int bodyPartNumber = position / 12;
+        int listIndex = position - 12 * bodyPartNumber;
+
+        switch (bodyPartNumber) {
+            case 0:
+                headIndex = listIndex;
+                break;
+            case 1:
+                bodyIndex = listIndex;
+                break;
+            case 2:
+                legIndex = listIndex;
+                break;
+            default:
+                break;
+        }
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("headIndex",headIndex);
+        bundle.putInt("bodyIndex",bodyIndex);
+        bundle.putInt("legIndex",legIndex);
+
+        final Intent intent = new Intent(this,AndroidMeActivity.class);
+        intent.putExtras(bundle);
+
+
+        final Button nextButton =  (Button)findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
 
     }
 }
